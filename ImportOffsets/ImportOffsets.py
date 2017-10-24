3d5f5daa293cf1475be98ab22072db542f9f6daa
Robert Marchese <bobm123@gmail.com>
2017-10-24 08:18:31 -0400

    Adds boilerplate code for F360 script

---
+++ ImportOffsets/ImportOffsets.py
@@ -0,0 +1,15 @@
+#Author-Robert Marchese
+#Description-Generated a 3D moded of a boat hull from a table of offsets
+
+import adsk.core, adsk.fusion, adsk.cam, traceback
+
+def run(context):
+    ui = None
+    try:
+        app = adsk.core.Application.get()
+        ui  = app.userInterface
+        ui.messageBox('Hello script')
+
+    except:
+        if ui:
+            ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
