206c9e168793b0c303f541880785afc62af90be2
Robert Marchese <bobm123@gmail.com>
2017-11-04 11:32:18 -0400

    Adds some more functionality to non-pandas csv reader

--- test_csv_open.py
+++ test_csv_open.py
@@ -10,8 +10,9 @@ __license__ = "MIT"
 
 import argparse
 import csv
+import json
 import logging
-
+import os
 
 # Setup Logging
 logger = logging.getLogger('csv test')
@@ -44,16 +45,53 @@ def fie_to_di(d):
         return d
 
 
+def get_all_axis(table, axis):
+    ''' return all rows with a specific axis (width, height, length)
+    in a dictionary and set 'name' as the key '''
+    selected = {r[1]:r[2:] for r in table if axis in r[0]}
+    return selected
+
+
+def is_valid(point):
+    return all(isinstance(w, float) for w in point)
+
+
+def remove_invalid(plist, replace=None):
+    if replace == None:
+        return [p for p in plist if is_valid(p)]
+    else:
+        return [p if is_valid(p) else replace for p in plist]
+
+
+def generate_sections(offset_table):
+    '''Generate the cross sections in 3D at each station
+    from the given lines and organizes the lines into coordinates
+    (x, y, z) = (width, height length). Returns each as a list
+    contained in a dictionary'''
+
+    # Have a dictionary with line names for keys, list of points for values
+    line_order = ['coaming', 'gunwale', 'sheer', 'chine', 'bottom', 'skeg']
+    sections = []
+    for line_name in line_order:
+        if line_name in offset_table:
+            sections.append(offset_table[line_name])
+
+    sections = list(map(list, zip(*sections)))
+    return sections
+
 
 def offset_reader(args):
     """ parse the csv expected offset table fields """
     logger.info("arguments" + str(args))
     with open(args.filename, 'r') as csvfile:
-        offset_table = list(csv.reader(csvfile, delimiter=',', quotechar='"'))
+        raw_table = list(csv.reader(csvfile, delimiter=',', quotechar='"'))
 
     logger.info('original table:')
-    for i, row in enumerate(offset_table):
+    for i, row in enumerate(raw_table):
         logger.info (f'row {i}: {row}')
+
+    # Remove comments lines
+    offset_table = [r for r in raw_table if not r[0].startswith('#')]
 
     # TODO: definitly 'happy path' coding here, so need to deal with
     # unexpected table formats.
@@ -81,11 +119,32 @@ def offset_reader(args):
     for i, row in enumerate(offset_table):
         logger.info (f'row {i}: {row}')
 
+    # Break out each set of dimension and recombine as (x,y,z)
+    ot_widths = get_all_axis(offset_table, 'width')
+    ot_heights = get_all_axis(offset_table, 'height')
+    ot_lengths = get_all_axis(offset_table, 'length')
 
+    ot_combined = {}
+    for line_name in ot_widths:
+        x = ot_widths[line_name]
+        y = ot_heights[line_name]
+        z = [float(zs) for zs in ot_lengths['station']]
+        line_points = remove_invalid(list(zip(x, y, z)), [])
+        ot_combined[line_name] = line_points
+
+    return ot_combined
 
 
 def main(args):
-    offset_reader(args)
+    offset_table = offset_reader(args)
+
+    offset_table['sections'] = generate_sections(offset_table)
+
+    out_filename, _ = os.path.splitext(args.filename)
+    out_filename = out_filename + '.json'
+    logger.debug('writing json data:\n' + json.dumps(offset_table))
+    with open(out_filename, 'w') as opf:
+        json.dump(offset_table, opf)
 
 
 if __name__ == "__main__":
