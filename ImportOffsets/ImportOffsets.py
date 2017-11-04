29d5184650ac62d41a04fd3dbb057dc2d7a080ed
Robert Marchese <bobm123@gmail.com>
2017-11-04 19:03:03 -0400

    Add rake angle code from csv parsing tool, remove commented code

--- ImportOffsets/ImportOffsets.py
+++ ImportOffsets/ImportOffsets.py
@@ -3,34 +3,26 @@
 
 import adsk.core, adsk.fusion, adsk.cam, traceback
 import json
+from math import isnan, radians, sin, cos
 
 
-def add_spline(point_list, sketch):
+def add_spline(point_list, sketch, mirror = 1):
     '''Adds a spline to the current drawing given an set of points in 3-space'''
 
     # Create an object to store the points in
     points = adsk.core.ObjectCollection.create()
-
     for p in point_list:
         if p:
-            points.add(adsk.core.Point3D.create(*p))
+            points.add(adsk.core.Point3D.create(mirror*p[0], p[1], p[2]))
 
     # Create the spline.
     sketch.sketchCurves.sketchFittedSplines.add(points)
-
-    # Do again for other side (TODO: Mirror?)
-    points1 = adsk.core.ObjectCollection.create()
-    for p in point_list:
-        if p:
-            points1.add(adsk.core.Point3D.create(-p[0], p[1], p[2]))
-
-    sketch.sketchCurves.sketchFittedSplines.add(points1)
 
     return points
 
 
 def add_cross_section(sketch, point_list, mirror = 1):
-    '''Adds a spline to the current drawing given an set of points in 3-space'''
+    '''Adds a polygon for a list of cross section verticies'''
 
     # TODO: generalize drawing a polygon from list of 2D points
     lines = sketch.sketchCurves.sketchLines;
@@ -64,7 +56,7 @@ def add_offset_plane (comp, sketch, z):
 
 
 def add_plane_by_points(comp, sketch, p0, p1, p2):
-     
+ 
     # TODO: ensure 3 points exist and not collinear
     # TODO: somehow use existing points
     sketchPoints = sketch.sketchPoints
@@ -101,7 +93,8 @@ def add_plane_at_an_angle(comp, sketch, section, angle):
 
 
 def scale_coordinates(in_list, scale):
-    # TODO: make this a numpy array operation
+    ''' Apply a scale factor to all the values in a list '''
+
     out_list = []
     for point in in_list:
         out_list.append([scale * a for a in point])
@@ -109,24 +102,73 @@ def scale_coordinates(in_list, scale):
     return out_list
 
 
+def rotate_point(cy, cz, angle, p):
+    '''rotate by an angle around a point in the yz-plane'''
+    s = sin(angle)
+    c = cos(angle)
+
+    # translate point back to origin:
+    p[1] -= cy
+    p[2] -= cz
+
+    # rotate point
+    ynew = p[2] * s + p[1] * c
+    znew = p[2] * c - p[1] * s
+
+    # translate point back:
+    p[1] = ynew + cy
+    p[2] = znew + cz
+
+    return p;
+
+
 def rake_angle(offsets, st_index, angle):
-    print("modifying section points: " + str(offsets['sections'][st_index]))
-    return offsets
+    ''' Apply a rake angle at the give section (usually bow or transom)'''
+
+    xc_original = offsets['sections'][st_index]
+    #logger.debug("original section " + str(st_index) + " points\n" + str(xc_original))
+
+    # Angle is given in degrees from the baseline
+    angle = radians(angle)
+
+    # Assume angle taken at top of section
+    y0 = xc_original[0][1]
+    z0 = xc_original[0][2]
+
+    # Apply rotation in xz plane around y = y0 to sections
+    xc_new = []
+    for pt in xc_original:
+        pt = list(pt)
+        pt = rotate_point(y0, z0, angle, pt)
+        xc_new.append(pt)
+    offsets['sections'][st_index] = xc_new
+
+    #logger.debug("modified section " + str(st_index) + " points\n" + str(xc_new))
+
+    # Apply rotation in xz plane around y = y0 to lines
+    for name,coords in offsets['lines'].items():
+        if coords[st_index]:
+            #logger.debug("modifying " + str(name) + " at station " + str(st_index))
+            pt = list(coords[st_index])
+            pt = rotate_point(y0, z0, angle, pt)
+            coords[st_index] = pt
+        #else:
+        #    logger.debug("ignoring " + str(name) + " at station " + str(st_index))
+
+    return offsets 
 
 
 def draw_offsets(design, offset_data):
-    # Get the root component of the active design.
+    ''' Draw the lines and sections represented by the offset table
+    on a new component '''
+    # Create a new component.
     rootComp = design.rootComponent
-
-    # Create a new occurrence.
     trans = adsk.core.Matrix3D.create()
     occ = rootComp.occurrences.addNewComponent(trans)
     newComp = occ.component
 
     # Create a new sketch on the xy plane.
     sketch = newComp.sketches.add(rootComp.xYConstructionPlane)
-
-    #TODO: Verify input file
 
     # Apply optional rake angles at bow and transom
     bindex = 0
@@ -137,14 +179,14 @@ def draw_offsets(design, offset_data):
     # Create a spline (two of them actually) for each line
     for name,coords in offset_data['lines'].items():
         coords = scale_coordinates(coords, .1) # mm to cm
-        add_spline(coords, sketch)
+        add_spline(coords, sketch, 1)
+        add_spline(coords, sketch,-1)
 
-    # Create construction planes for the cross sections
+    # Create the cross sections
     for i,section in enumerate(offset_data['sections']):
         section = scale_coordinates(section, .1) # mm to cm
         #newConstPlane = add_offset_plane(newComp, sketch, section[0][2])
         #newSketch = newComp.sketches.add(newConstPlane)
-        #add_cross_section(newSketch, section)
         add_cross_section(sketch, section, 1)
         add_cross_section(sketch, section,-1)
 
@@ -167,7 +209,6 @@ def get_user_file(ui):
     fileDlg.filter = '*.json'
     dlgResult = fileDlg.showOpen()
     if dlgResult == adsk.core.DialogResults.DialogOK:
-        #for filename in fileDlg.filenames:
         user_file = fileDlg.filenames[0]
         return user_file
     else:
@@ -179,39 +220,11 @@ def run(context):
     try:
         app = adsk.core.Application.get()
         ui  = app.userInterface
-        #ui.messageBox('Hello script')
 
         design = app.activeProduct
         if not design:
             ui.messageBox('No active Fusion 360 design', 'No Design')
             return
-
-        # Get the root component of the active design.
-        #rootComp = design.rootComponent
-
-        # Create a new occurrence.
-        #trans = adsk.core.Matrix3D.create()
-        #occ = rootComp.occurrences.addNewComponent(trans)
-
-        # Get the associated component.
-        #newComp = occ.component
-
-        # Create a new sketch on the xy plane.
-        #sketch = newComp.sketches.add(rootComp.xYConstructionPlane)
-
-        # Get offsets from the user's file
-        #msg = ''
-        # Set styles of file dialog.
-        #fileDlg = ui.createFileDialog()
-        #fileDlg.isMultiSelectEnabled = False
-        #fileDlg.title = 'Open'
-        #fileDlg.filter = '*.json'
-        #dlgResult = fileDlg.showOpen()
-        #if dlgResult == adsk.core.DialogResults.DialogOK:
-        #    #for filename in fileDlg.filenames:
-        #    json_file = fileDlg.filenames[0]
-        #    with open(json_file, 'r') as f:
-        #        offset_data = json.load(f)
 
         user_file = get_user_file(ui);
         if user_file:
@@ -221,33 +234,7 @@ def run(context):
             return
 
         #TODO: Verify input file
-
         draw_offsets(design, offset_data)
-
-        # Apply optional rake angles at bow and transom
-        #bindex = 0
-        #offset_data = rake_angle(offset_data, bindex, 18)
-        #tindex = len(offset_data['sections']) - 1
-        #offset_data = rake_angle(offset_data, tindex, -25)
-
-        # Create a spline (two of them actually) for each line
-        #for name,coords in offset_data['lines'].items():
-        #    coords = scale_coordinates(coords, .1) # mm to cm
-        #    add_spline(coords, sketch)
-
-        # Create construction planes for the cross sections
-        #for i,section in enumerate(offset_data['sections']):
-        #    section = scale_coordinates(section, .1) # mm to cm
-        #    #newConstPlane = add_offset_plane(newComp, sketch, section[0][2])
-        #    #newSketch = newComp.sketches.add(newConstPlane)
-        #    #add_cross_section(newSketch, section)
-        #    add_cross_section(sketch, section, 1)
-        #    add_cross_section(sketch, section,-1)
-
-        # Testing orientation of angled planes
-        #for i,section in enumerate(offset_data['sections']):
-        #    section = scale_coordinates(section, .1) # mm to cm
-        #    newConstPlane = add_plane_at_an_angle(newComp, sketch, section, -25)
 
     except:
         if ui:
